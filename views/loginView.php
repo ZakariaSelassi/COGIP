794775821a7dfb4a87ae7ba21f9651e033abbc3d
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2021-04-23 10:34:12 +0200

    up css

--- views/loginView.php
+++ views/loginView.php
@@ -1,29 +1,31 @@
-<html>
-   <head>
-   </head>
-   <body>
-  
-      <form action="" method="POST">
-        <p>
-        <label> username
-        </label>
-      <input id="username" name="username" type="text" required="required"/>
-      <br>
-        </p>
-        <p>
-      <label> Password
-      </label>
-      <input id="password" name="password" type="password" required="required"/>
-        </p>
-      <br/>
-      <p>
-        <button type="submit"><span>Login</span><button>
-        <button type="reset"><span>Cancel</span><button>
-      </p>
-        </form>
-    </body>
 
+  <?php $tittle = 'login'; ?>
+  <?php ob_start(); ?>
 
+  <div class="container" style="margin-top: 50px">
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <div class="card p-3 login-card mx-auto">
+          <h5 class="text-center login-card-title">COGIP.app</h5>
+          <br />
+          <form id="login-form" class="form" action="" method="post">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="User">
+            </div>
+            <div class="form-group">
+              <input id="password-login" type="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group2 text-center">
+              <input type="submit" name="submit" class="btn btn-primary btn-block" value="submit">
+              <input type="reset" name="reset" class="btn btn-primary btn-block" value="Cancel">
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+  <?php $content = ob_get_clean(); ?>
 
-
-</html>
+  <?php require('template.php');?> 
