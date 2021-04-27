c8602382c8d7c04f9d7c0644574f9ae8fc6c3a4d
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2021-04-27 10:27:32 +0200

    Update loginController.php

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -1,18 +1,44 @@
 <?php
-    include_once('./model/login.php');
-class Controller extends Model{
+namespace App\model;
 
-    public function invoke()
-    {
-        $reslt = $this->getlogin();
+use PDO;
+
+use App\model\classe\Database;
+
+class Controller extends database{
+
+
+    function public checkLogin(){
         
-        if($reslt == 'login')
-        {
-            include './views/afterlogin.php';
+        if (isset($login) && isset($pwd)) {
+            $login = $_POST["login"];
+            $pwd = $_POST["pwd"];
+            
+            $login_valide = $bdd -> query("SELECT Identifiant FROM utilisateur WHERE Identifiant = '$login' ");
+            $pwd_valide = $bdd -> query("SELECT Identifiant FROM utilisateur WHERE Identifiant = '$pwd' ");
+
+            // on vérifie les informations du formulaire, à savoir si le pseudo saisi est bien un pseudo autorisé, de même pour le mot de passe
+            if ($login_valide && $pwd_valide) {
+                // dans ce cas, tout est ok, on peut démarrer notre session
+
+                // on la démarre :)
+                session_start ();
+                // on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
+                $_SESSION['login'] = $_POST['login'];
+                $_SESSION['pwd'] = $_POST['pwd'];
+
+                // on redirige notre visiteur vers une page de notre section membre
+                header ('location: /home');
+            }
+            else {
+                // Le visiteur n'a pas été reconnu comme étant membre de notre site. On utilise alors un petit javascript lui signalant ce fait
+                echo '<body onLoad="alert(\'Membre non reconnu...\')">';
+                // puis on le redirige vers la page d'accueil
+                echo '<meta http-equiv="refresh" content="0;URL=Read.php">';
+            }
         }
-        else
-        {
-            include './views/loginView.php';
+        else {
+            echo 'Les variables du formulaire ne sont pas déclarées.';
         }
     }
 }
